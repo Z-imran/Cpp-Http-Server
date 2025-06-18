c550f096398d8ec59abfddad877a266981660f51
Z-imran <148902089+Z-imran@users.noreply.github.com>
2025-06-18 03:47:36 -0600

    Started adding Response objects specifications. Will later add further Specifications and update Server logic.

--- server/Response.hpp
+++ server/Response.hpp
@@ -1,3 +1,6 @@
+#include <string>
+#include <unordered_map>
+#include "Request.hpp"
 #ifndef RESPONSE_UNIQUE
 #define RESPONSE_UNIQUE
 
@@ -5,7 +8,42 @@
 // a handleResponse function and a toHTTP function that handles the incoming request to convert it 
 // into a valid HTTP response. 
 
+class Response {
+    public: 
+        
+    int status_code;                                        // The code corrresponding to the status of the response.
+    std::string status_message;                             // The correspinsding status meesage.
+    std::unordered_map<std::string, std::string> headers;   // The Key-value of header fields.
+    std::string body;                                       // The extra information.
 
+
+    /**
+     * @brief constructor/deconstructor for a Response. 
+     * 
+     * Creates/Destroys the resposne object.
+     * 
+     */
+    Response();
+    ~Response();
+
+
+    /**
+     * @brief Builds the response object based on the input request. 
+     * 
+     * Makes the response object contain the correct values for the corresponding variables. Uses a map for common status codes. 
+     * If the status code is not common defaults to generic message. 
+     * 
+     */
+    Response buildResponse();
+
+
+    /**
+     * @brief Converts the Response into a valid string for HTTP repsonse. 
+     * 
+     * Formats a string to be a valid HTTP response. 
+     */
+    std::string toHTTP();
+};
 
 
 
